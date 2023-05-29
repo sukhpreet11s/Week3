d30a78a490240770a5f92535f5067eb3fde4a81e
sukhp <sukhp@10.48.3.57>
2023-05-29 12:27:49 -0400

    version 1

---
+++ src/week3/Week3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3;
+
+/** Local rep - steps 1. initilize versoining , .gitconfig , .gitignore. 4. commit
+ * 5. push the code to github repository 
+ *
+ * @author sukhp
+ */
+public class Week3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("learning github operations");
+    }
+    
+}
